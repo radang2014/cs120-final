73f905b95d0268217f2adff46d116039e20b4b4a
Randy A. Dang <rdang02@vm-hw09.eecs.tufts.edu>
2024-04-19 17:10:24 -0400

    Add template app

---
+++ template-app.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var port = 8080;
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>HTML Template</h1>");
+    res.write("<p>Hello World!</p>");
+    res.end()
+}).listen(port)
+
