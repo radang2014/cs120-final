eafef924586c7eaab615dd3724dbaa2743c7fb37
radang2014 <davidjonescomorgnetus@gmail.com>
2024-04-26 00:54:45 -0400

    remove extraneous files

--- common_module.js
+++
@@ -1,9 +0,0 @@
-/* 
- * Node module for common functionality between pages on app 
- */
-
-/* Dummy function for testing */
-exports.print_hello = function (req, res) {
-    res.write("<p>Hello World!</p>");
-};
-
