76cecbc794fe23cfb8345abfd5eb77b198116109
Alyssa <alyssa.bawgus@gmail.com>
2024-04-28 16:49:15 -0400

    Added browse event links

--- my_modules/browse_events.js
+++ my_modules/browse_events.js
@@ -13,11 +13,9 @@ exports.serve_events_content = async function(req, res) {
 
     await mongo_query.get_account_info(req, res, current_user)
     .then(async user_info =>{
-        console.log(user_info)
         return await mongo_query.get_near_events(req, res, user_info.zip_code)
     })
     .then(async events => {
-        console.log(events)
         var stn = ''
         
         events.forEach(event =>{
@@ -28,11 +26,13 @@ exports.serve_events_content = async function(req, res) {
             let datetime = event.event_date.
                 replace(/T/, ' ').      // replace T with a space
                 replace(/\..+/, '')
-            stn += `<div class="browse_event_item" id='${event._id.toString()}'>
+            stn += `<button class="browse_event_item" id='${event._id.toString()}'
+                    onclick="location.href='./event?event=${event._id.toString()}'"
+                    >
                     ${event.owner_info[0].firstname}'s ${event.tag[0]} Event<br>
                     At ${event.loc[0].name}, ${datetime}<br>
                     ${event.attendees.length} ${noun} going <br>
-                    </div>`
+                    </button>`
         })
         $('#event_container').html(stn);
     })
