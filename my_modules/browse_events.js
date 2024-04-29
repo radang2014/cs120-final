534e188c798fed81c8824f18e3949230e2914dbc
Alyssa <alyssa.bawgus@gmail.com>
2024-04-29 18:04:08 -0400

    added icons & changed content of browse event divs

--- my_modules/browse_events.js
+++ my_modules/browse_events.js
@@ -18,21 +18,27 @@ exports.serve_events_content = async function(req, res) {
     })
     .then(async events => {
         var stn = ''        
-        events.forEach(event =>{
-            var noun = 'person'
-            if (event.attendees.length > 1) {
-                noun = 'people'
+        events.forEach(event =>{            
+            let optionalText = ''
+            if (event.attendees.length == 2) {                
+                optionalText = ` & 1 other`
+            } else if (event.attendees.length > 2) {
+                optionalText = ` $ ${event.attendees.length-1} other`
             }
             let datetime = event.event_date.toISOString().
                 replace(/T/, ' ').
                 replace(/\..+/, '')
             stn += `<button class="browse_event_item" id='${event._id.toString()}'
                 onclick="location.href='./event?event=${event._id.toString()}'">
-                <p class="Username">${event.owner_info[0].firstname}</p>
+                
                 <p class="EventTag">${event.tag[0]} Event</p>
+                <p class="exerciseTypeIcon"><img src="pages/images/${event.tag[0].toLowerCase()}.png" width="50px" height="50px" alt="${event.tag[0]}"></p>
                 <p class="EventLocation">At ${event.loc[0].name}</p>
                 <p class="EventLocation"> ${datetime} </p>
-                <p class="EventAttendees"> ${event.attendees.length} <span class="noun">${noun}</span> going</p>
+                <br>                
+                <p class="EventDescription"> "${event.description}"</p>                                
+                <br><br><br>
+                <p class="EventAttendees"> <b>${event.owner_info[0].firstname}</b>${optionalText} </span> going</p>
                 </button>`;
         })
         $('#event_container').html(stn);
@@ -40,5 +46,4 @@ exports.serve_events_content = async function(req, res) {
     .then( _ =>        {
         res.write($.html())
     })
-
 }
